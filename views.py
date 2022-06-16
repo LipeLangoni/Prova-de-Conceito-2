5e2e147376198cfaea8ef9e0a405f7a54168195a
Felipe Langoni Ramos <65926064+LipeLangoni@users.noreply.github.com>
2022-06-16 01:28:48 -0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,23 @@
+
+from flask import render_template
+from app import app
+from datetime import datetime
+from flask import Flask, request, render_template_string
+
+@app.route('/')
+def home():
+   return "hello world! fodase em dobro"
+
+@app.route('/template')
+def template():
+    return render_template('home.html')
+@app.route('/hora')
+def hora():
+    return str(datetime.now())
+
+@app.route('/soma')
+def sum():
+	return 2+2
+
+app.run()
+
